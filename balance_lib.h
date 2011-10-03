5bde5202cef90af8419eaffac05454a6d72057d6
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-10-04 01:27:02 +0200

    remove forgotten function declarations

--- balance_lib.h
+++ balance_lib.h
@@ -1,7 +1,3 @@
 /* balance_lib.c */
 unsigned char *balance_u8(unsigned char *data, size_t size, size_t nb_min, size_t nb_max);
 float *balance_f32(float *data, size_t size, size_t nb_min, size_t nb_max);
-float *idiv_f32(float *a, const float *b, size_t size);
-float *imul_f32(float *a, const float *b, size_t size);
-float *imuls_f32(float *a, const float b, size_t size);
-float *iadd_f32(float *a, const float *b, size_t size);
