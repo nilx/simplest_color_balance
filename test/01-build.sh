af9ddfb843936794facf2edf9afaaeec65e59d11
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-10-26 20:50:27 +0900

    test with i586-mingw32msvc-cc (Win32 ABI)
    - headers and DLLs from gnuwin32 packages
    - compile with mingw
    - run with wine

--- test/01-build.sh
+++ test/01-build.sh
@@ -34,18 +34,30 @@ _log make clean
 _log make
 
 echo "* compiler support"
-for CC in cc c++ c89 c99 gcc g++ tcc nwcc clang icc pathcc suncc; do
+for CC in cc c++ c89 c99 gcc g++ tcc nwcc clang icc pathcc suncc \
+    i586-mingw32msvc-cc; do
     which $CC || continue
     echo "* $CC compiler"
     _log make distclean
     case $CC in
+	"i586-mingw32msvc-cc")
+	    test -d ./win32/lib || break
+	    # default mingw behaviour has extra precision
+	    _log make CC=$CC CFLAGS="-O2 -ffloat-store"\
+		CPPFLAGS="-DNDEBUG -I. -I./win32/include" \
+		LDFLAGS="-L./win32/lib"
+	    ln -f -s ./win32/bin/libpng3.dll ./win32/bin/zlib1.dll ./
+	    ;;
 	"icc")
 	    # default icc behaviour is wrong divisions!
-	    _log make CC=$CC CFLAGS="-fp-model precise";;
+	    _log make CC=$CC CFLAGS="-fp-model precise"
+	    ;;
 	*)
-	    _log make CC=$CC ;;
+	    _log make CC=$CC
+	    ;;
     esac
     _log _test_run
+    rm -f *.dll
 done
 
 _log make distclean
